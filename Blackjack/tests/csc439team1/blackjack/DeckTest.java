acbe61c82d0f0e389ae2c47413dba26aae1e3264
Conner Martin <55250241+Martinc11@users.noreply.github.com>
2020-09-27 20:04:17 -0400

    These provide 100% coverage. Comment's added.

--- Blackjack/tests/csc439team1/blackjack/DeckTest.java
+++ Blackjack/tests/csc439team1/blackjack/DeckTest.java
@@ -2,20 +2,28 @@ package csc439team1.blackjack;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 
 public class DeckTest
 {
 
+    /**
+     * Tests deck's pick() and size method. Pick 5 cards and verify that size of deck is of size 47. Tests size method as well.
+     */
     @Test
     public void pick()
     {
-        /*Deck deck0 = new Deck();
-        System.out.println(deck0.toString());*/
+        Deck deck0 = new Deck();
+        for(int i = 0; i <= 4; i++){
+            deck0.pick();
+        }
+        assertEquals(deck0.size(), 47);
     }
 
     /**
-     * Tests exception handling in pick()
+     * Tests exception handling in pick(). Remove all cards from deck with pick() method, then call pick() again which should throw an exception.
      */
     @Test (expected = IllegalArgumentException.class)
     public void pick1() {
@@ -24,10 +32,11 @@ public class DeckTest
             deck1.pick();
         }
         deck1.pick();
+
     }
 
     /**
-     * Tests testToString().
+     * Tests testToString(). First test: toString should contain the expected string. Second test: card removed at random. Deck's toString should then not contain the card's toString value.
      */
     @Test
     public void testToString()
@@ -42,13 +51,4 @@ public class DeckTest
 
     }
 
-    @Test
-    public void size()
-    {
-    }
-
-    @Test
-    public void isEmpty()
-    {
-    }
 }
