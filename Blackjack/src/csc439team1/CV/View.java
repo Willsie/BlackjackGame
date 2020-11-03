bbc18795f992eb027ccce86bd006c351ca7efde7
Trevor Sears <searstrev@fuse.net>
2020-11-02 22:15:26 -0500

    Updated local repo, and my branch to be inline with group master branch. Added private methods checkInt, checkQuit to Controller; which allows for checking int conversion and checking for quit as a method call instead of if clause in other methods. Will add more detailed comments and javadoc if group wants to use.
    Signed-off-by: Trevor Sears <searsTrev@fuse.net>

--- Blackjack/src/csc439team1/CV/View.java
+++
@@ -1,6 +0,0 @@
-package csc439team1.CV;
-
-public abstract class View {
-    public abstract void purchasePrompt();
-    public abstract int purchaseAmount();
-}
--- Blackjack/src/csc439team1/blackjack/controller/Controller.java
+++ Blackjack/src/csc439team1/blackjack/controller/Controller.java
@@ -5,6 +5,10 @@ import csc439team1.blackjack.model.Player;
 import csc439team1.blackjack.model.Shoe;
 import csc439team1.blackjack.view.View;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
+
 public class Controller {
     View view;
     Player player = new Player();
@@ -20,11 +24,14 @@ public class Controller {
         askBet();
         initialDeal(shoe);
     }
-
+    //example of changed method
     public void buyChips() {
         view.output("Game is starting.....how much chips do you want to buy (between $10 to $5000): ");
         String input = view.input();
-        if (input.equals("quit")) quit();
+        //call to method which checks for quit
+        checkQuit(input);
+        //call to method which does try catch via console to ensure converts to int
+        input = checkInt(input);
         int amount = Integer.parseInt(input);
         if (amount > 5000 || amount < 10) {
             throw new IllegalArgumentException();
@@ -35,7 +42,8 @@ public class Controller {
     public void askBet() {
         view.output("How much do you want to bet (between $10 to $500): ");
         String input = view.input();
-        if (input.equals("quit")) quit();
+        checkQuit(input);
+        input = checkInt(input);
         int bet = Integer.parseInt(input);
         if (bet > player.getChips() || bet > 500 || bet < 10) {
             throw new IllegalArgumentException();
@@ -57,5 +65,32 @@ public class Controller {
     public void quit() {
         view.output("Player has quit");
         System.exit(0);
+    }
+    //private method runs try catch while input cannot convert, and returns acceptable string
+    //or lets player quit
+    private String checkInt (String input)
+    {
+        boolean isInt = false;
+        while(!isInt)
+        {
+            checkQuit(input);
+            try{
+                Integer.parseInt(input);
+                isInt = true;
+            }
+            catch (Exception e)
+            {
+                view.output("Entry must be an integer, try again\n");
+                input = view.input();
+
+            }
+        }
+        return input;
+    }
+    //private method which calls quit if input string equals quit regardless of letter case
+    private void checkQuit(String input){
+        if (input.toLowerCase().equals("quit")){
+            quit();
+        }
     }
 }
