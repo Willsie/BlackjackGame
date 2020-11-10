865b37be5b35b8319fe82b68c446db25d164db29
Trevor Sears <searsTrev@fuse.net>
2020-11-10 14:23:20 -0500

    Added validation checks to PLayer and Dealer (for chips/cards related methods), plus updated validation checks to Controller (javadocs still not added as of yet) with try catch addded to methods dealing with chips and cards.
    Signed-off-by: Trevor Sears <searsTrev@fuse.net>

--- Blackjack/src/csc439team1/blackjack/Blackjackgame.java
+++ Blackjack/src/csc439team1/blackjack/Blackjackgame.java
@@ -1,6 +1,7 @@
 package csc439team1.blackjack;
 
 import csc439team1.blackjack.controller.Controller;
+import csc439team1.blackjack.model.Dealer;
 import csc439team1.blackjack.view.CLIView;
 
 public class Blackjackgame {
