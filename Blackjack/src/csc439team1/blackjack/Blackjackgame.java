a1270dab7bc57d2803724f6f14f4c3766277271a
Conner Martin <55250241+Martinc11@users.noreply.github.com>
2020-11-10 21:08:21 -0500

    Revised controller and view and added JavaDocs to some methods. Other's may still need to change so I left the comments as is in those.

--- Blackjack/src/csc439team1/blackjack/Blackjackgame.java
+++ Blackjack/src/csc439team1/blackjack/Blackjackgame.java
@@ -6,8 +6,13 @@ import csc439team1.blackjack.view.CLIView;
 
 public class Blackjackgame {
     public static void main (String[] args){
-        CLIView view = new CLIView();
-        Controller controller = new Controller(view);
-        controller.playBlackjack();
+        try{
+            CLIView view = new CLIView();
+            Controller controller = new Controller(view);
+            controller.playBlackjack();
+        } catch (IllegalStateException e) {
+            System.exit(0);
+        }
+
     }
 }
--- Blackjack/tests/csc439team1/blackjack/controller/ControllerTest.java
+++ Blackjack/tests/csc439team1/blackjack/controller/ControllerTest.java
@@ -1,5 +1,6 @@
 package csc439team1.blackjack.controller;
 
+
 import static org.junit.Assert.*;
 
 public class ControllerTest {
