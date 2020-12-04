f458f77683c1d2e7a854432b36c4eeec49dc0c5a
Trevor Sears <searsTrev@fuse.net>
2020-12-04 15:18:50 -0500

    Added missing javaDoc for logger declaration in Blackjackgame.java.
    Signed-off-by: Trevor Sears <searsTrev@fuse.net>

--- Blackjack/src/csc439team1/blackjack/Blackjackgame.java
+++ Blackjack/src/csc439team1/blackjack/Blackjackgame.java
@@ -10,13 +10,16 @@ import java.util.logging.Logger;
  */
 public class Blackjackgame {
     /**
+     * Logger for Blackjackgame class.
+     */
+    private static final Logger logger = Logger.getLogger(Blackjackgame.class.getName());
+    /**
      * Main starts the Blackjack game by instantiating a CLIview object and passing it to controller's constructor. Then the playBlackJack method is invoked.
      * This is in a try catch block because quit() throws an exception instead of calling System.exit(0) itself.
      * This was done to facilitate testing of the quit functionality in controller as well as provide a clean quit scenario to the user.
      *
      * @param args
      */
-    private static final Logger logger = Logger.getLogger(Card.class.getName());
     public static void main(String[] args) {
         logger.entering("Blackjackgame", "main");
         try {
